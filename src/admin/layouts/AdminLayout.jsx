b401854d2298300dc78f8aa05d6ac325a452b5f6
kri650 <kritikathakur24@navgurukul.org>
2026-05-29 00:16:14 +0530

    fix: add missing useState import in AdminLayout

--- src/admin/layouts/AdminLayout.jsx
+++ src/admin/layouts/AdminLayout.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { Outlet, useNavigate } from 'react-router-dom';
 import Sidebar from '../components/Sidebar';
 import Topbar from '../components/Topbar';
